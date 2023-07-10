b26415191682168b7a0c4a3e034253b3642c00bf
davidcoats <david@getsafetycone.com>
2023-07-10 13:32:08 -0700

    Interim changes.

---
+++ source/R5T.L0035/Code/Functionality/IVersionOperator.cs
@@ -0,0 +1,14 @@
+using System;
+
+using R5T.T0132;
+
+
+namespace R5T.L0035
+{
+    [FunctionalityMarker]
+    public partial interface IVersionOperator : IFunctionalityMarker,
+        L0034.IVersionOperator
+    {
+
+    }
+}
---
+++ source/R5T.L0035/Code/ΩInstances/VersionOperator.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace R5T.L0035
+{
+    public class VersionOperator : IVersionOperator
+    {
+        #region Infrastructure
+
+        public static IVersionOperator Instance { get; } = new VersionOperator();
+
+
+        private VersionOperator()
+        {
+        }
+
+        #endregion
+    }
+}
